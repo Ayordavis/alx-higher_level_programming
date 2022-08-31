1bd31ab0f63d68b4ba62d704ce641f44564740ae
Ayordavis <102733939+Ayordavis@users.noreply.github.com>
2022-08-31 13:31:44 +0100

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,6 +3,7 @@
 
 /**
  * print_python_bytes - gives data of the PyBytesObject
+ *
  * @p: the PyObject
  */
 
@@ -35,12 +36,13 @@ void print_python_bytes(PyObject *p)
 
 /**
  * print_python_list - gives data of the PyListObject
+ *
  * @p: the PyObject
  */
 
 void print_python_list(PyObject *p)
 {
-	py_ssize_t size = 0;
+	Py_ssize_t size = 0;
 	PyObject *item;
 	int i = 0;
 
